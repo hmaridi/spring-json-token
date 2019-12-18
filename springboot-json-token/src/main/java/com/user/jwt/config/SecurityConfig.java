93276b27a007e4d0d2d9d24ccc3661d1d2f34bbb
HARI KISHORE <42566418+devoxx123@users.noreply.github.com>
2019-12-18 18:06:23 +0530

    Update SecurityConfig.java

--- springboot-json-token/src/main/java/com/user/jwt/config/SecurityConfig.java
+++ springboot-json-token/src/main/java/com/user/jwt/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.core.userdetails.User;
 
 @Configuration
 @EnableWebSecurity
